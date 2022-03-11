684f952804389df00ce336571e42e85545825f42
omelian levkovych <omelianai@gmail.com>
2022-03-11 23:13:21 +0200

    added soft delete query extension method add query filter for every soft deleted entity

---
+++ src/Domain/Extensions/SoftDeleteQueryExtension.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities.Interfaces;
+using Microsoft.EntityFrameworkCore.Metadata;
+using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Domain.Extensions
+{
+    public static class SoftDeleteQueryExtension
+    {
+        public static void AddSoftDeleteQueryFilter(
+            this IMutableEntityType entityData)
+        {
+            var methodToCall = typeof(SoftDeleteQueryExtension)
+                .GetMethod(nameof(GetSoftDeleteFilter),
+                    BindingFlags.NonPublic | BindingFlags.Static)
+                .MakeGenericMethod(entityData.ClrType);
+
+            var filter = methodToCall.Invoke(null, new object[] { });
+            entityData.SetQueryFilter((LambdaExpression)filter);
+            entityData.AddIndex(entityData.
+                FindProperty(nameof(ISoftDelete.SoftDeleted)));
+        }
+
+        private static LambdaExpression GetSoftDeleteFilter<TEntity>()
+            where TEntity : class, ISoftDelete
+        {
+            Expression<Func<TEntity, bool>> filter = x => !x.SoftDeleted;
+            return filter;
+        }
+    }
+}
