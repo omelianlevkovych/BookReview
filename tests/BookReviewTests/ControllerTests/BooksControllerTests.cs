fe5d0e7e12295b506be0a1fec5389598f4acd135
omelian levkovych <omelianai@gmail.com>
2022-03-12 18:56:27 +0200

    removed fake test added first test for books controller minor cleanup

---
+++ tests/BookReviewTests/ControllerTests/BooksControllerTests.cs
@@ -0,0 +1,48 @@
+﻿using BookReview.Controllers;
+using BusinessLogic.Logic.Interfaces;
+using BusinessLogic.Models;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BookReview.Tests.ControllerTests
+{
+    public class BooksControllerTests
+    {
+        [Fact]
+        public async Task GetBooks_returns_books()
+        {
+            // Arrange.
+            var logicMock = new Mock<IBooksLogic>();
+
+            var books = new List<BookModel>
+            {
+                new BookModel
+                {
+                    Id = 1,
+                },
+                new BookModel 
+                { 
+                    Id = 2 
+                },
+            };
+
+            logicMock.Setup(x => x.GetAll())
+                .ReturnsAsync(books);
+
+            var controller = new BooksController(logicMock.Object);
+
+            // Act.
+            var result = await controller.GetBooks();
+
+            // Assert.
+            Assert.IsAssignableFrom<IEnumerable<BookModel>>(result);
+            Assert.Equal(2, result.Count());
+        }
+
+    }
+}
