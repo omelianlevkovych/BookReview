5efbc527912fe79d2fae9ca36fe5dbf2751cb146
omelian levkovych <omelianai@gmail.com>
2022-03-13 19:28:37 +0200

    added infrastructure added global error handling

---
+++ Infrastructure/Exceptions/CustomException.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Exceptions
+{
+    public abstract class CustomException : Exception
+    {
+        public CustomException()
+        { }
+
+        public CustomException(string message)
+            : base(message)
+        { }
+
+        public CustomException(string message, Exception inner)
+            : base(message, inner)
+        { }
+    }
+}
