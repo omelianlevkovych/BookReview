150d04e754c0470c3eed31e1cfdc25706bf66b59
$sudo whoami <omelianai@gmail.com>
2022-03-13 20:12:35 +0200

    Fixed solution structure (#20)
    * added infrastructure
    added global error handling
    
    * adjusted error message to status code
    
    * fixed solution structure
    
    * cleanup

---
+++ src/Infrastructure/Exceptions/NotFoundException.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure.Exceptions
+{
+    public class NotFoundException : CustomException
+    {
+        public NotFoundException()
+        { }
+
+        public NotFoundException(string message)
+            : base(message)
+        { }
+
+        public NotFoundException(string message, Exception inner)
+            : base(message, inner)
+        { }
+    }
+}
